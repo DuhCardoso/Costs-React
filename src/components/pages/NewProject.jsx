f7f386dc899e52c41e6fc9f7a6f80e5daae7cb7a
Eduardo C. de Jesus <edududu221@gmail.com>
2022-12-30 08:39:38 -0300

    Configuração: - Montagem da fetch POST para o bd "projects"

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,13 +1,37 @@
+import {useNavigate} from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm'
 
 import styles from './NewProject.module.css'
 
 export default function NewProject(){
+    const history = useNavigate()
+
+    function createPost(project){
+        // Intialize const and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects",{
+            method: 'POST',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                // Redirect
+                history.push('/project', {message: 'Projeto criado com sucesso!'})
+            })
+            .catch(e => console.log(e))
+    }
+
     return (
         <div className={styles.newProjectContainer}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicioner os serviços</p>
-            <ProjectForm btnText='Criar Projeto '/>
+            <ProjectForm handleSubmit={createPost} btnText='Criar Projeto '/>
         </div>
     )
 }
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -6,8 +6,9 @@ import SubmitButton from '../form/SubmitButton'
 
 import styles from './ProjectForm.module.css'
 
-export default function ProjectForm({btnText}){
+export default function ProjectForm({handleSubmit, btnText, projectData}){
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
@@ -23,25 +24,47 @@ export default function ProjectForm({btnText}){
             .catch((e) => console.log(e))
     }, [])
 
+    const submit = (e)=>{
+        e.preventDefault()
+        handleSubmit(project)
+        // console.log(project)
+    }
 
-    return(
-        <form className={styles.form} >
+    function handleChange(e){
+        setProject({ ...project, [e.target.name]: e.target.value})
+    }
+    
+    function handleCategory(e){
+        setProject({ ...project, category:{
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text
+        }})
+    }
+
+    return( 
+        <form onSubmit={submit} className={styles.form} >
             <Input 
                 type="text" 
                 text='Nome do projeto:' 
                 name='name'
                 placeholder='Insira o nome do projeto'
+                handleOnChange={handleChange}
+                value={project.name ? project.name : ''}
             />
             <Input 
                 type="number" 
                 text='Orçamento do projeto:' 
                 name='budget'
                 placeholder='Insira o orçamento total'
+                handleOnChange={handleChange}
+                value={project.budget ? project.budget : ''}
             />
             <Select 
                 name="categoryId" 
                 text='Selecione a categoria:' 
                 options={categories}
+                handleOnChange={handleCategory}
+                value={project.category ? project.category.id : ''}
             />
             <SubmitButton text={btnText}/>
         </form>
